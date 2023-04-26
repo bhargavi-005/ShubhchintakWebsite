ac2dce09957d326269eacf2fb35721bb362c21f2
Chinmay Bhat <0310chinmay@gmail.com>
2023-04-26 16:52:52 +0530

    section 2 completed

--- shubhchintak-india/src/pages/landing.js
+++ shubhchintak-india/src/pages/landing.js
@@ -1,4 +1,5 @@
 import bg from "../../public/images/bg.png";
+import Image from "next/image";
 function landing(){
     return(
         <div>
@@ -6,17 +7,23 @@ function landing(){
             <h1 className="relative mt-[8%] text-center text-white font-xyz text-[60px]">We are bringing</h1>
             <h1 className="relative text-center text-[#0F89FF] font-xyz text-[60px]">DIGITAL TRANSFORMATION</h1>
             <p className="relative text-center text-white text-[16px] ml-[20%] mr-[20%]">Future is for those who work for their dreams. Our deep knowledge and expertise in providing digital solutions 
-help the dreamers to take step forward to start with their dream business ideas.We accompany them at every 
-step to leverage innovation and advancement as per the latest market trends.</p>
+               help the dreamers to take step forward to start with their dream business ideas.We accompany them at every 
+               step to leverage innovation and advancement as per the latest market trends.</p>
             <p className="relative text-center text-white text-[16px] ml-[20%] mr-[20%]">We identify their challenge ,guide them, create effective work strategy, develop their complex 
-business ideas and deploy it as earliest as possible with the help of our expert team.</p>
+               business ideas and deploy it as earliest as possible with the help of our expert team.</p>
             <button class="ml-[33%] mt-[3%] bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-[100px] rounded-full">
              Start your digital transformation journey
           </button>
         </div>
         {/* Next Section */}
         <div className="mt-[2%]">
-            <h2 className="text-center text-[20px]">Our Partners</h2>
+            <h2 className="text-center text-[20px] font-abc">Our Partners</h2>
+            <div className="ml-[5%]">
+                    <Image
+                    src="/images/partners.png"
+                    height={700}
+                    width={1700}/>
+            </div>
            
         </div>
         </div>
