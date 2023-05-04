dae8dec12d2a08ca7a47575a00d3306b25278d27
Chinmay Bhat <0310chinmay@gmail.com>
2023-05-04 17:34:08 +0530

    updated files

--- shubhchintak-india/src/pages/partners.js
+++ shubhchintak-india/src/pages/partners.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 import Marquee from "react-fast-marquee";
 function partner(){
     return(
-        <div className=" bg-white pb-[3%]">
-            <h2 className=" text-center text-[20px] font-extrabold font-abc">OUR PARTNERS</h2>
+        <div className=" bg-white pb-[1%]">
+            <h2 className=" text-center text-[20px] font-extrabold pt-[2%] font-abc">OUR PARTNERS</h2>
             <div className="flex flex-row justify-evenly mt-[5%]">
                 <Marquee>
             <div className="mr-[70%]">
