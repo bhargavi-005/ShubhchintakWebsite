278d35ce5ef4b5d2de2736fb2daae29e02f0f006
Chinmay Bhat <0310chinmay@gmail.com>
2023-05-04 18:25:12 +0530

    Update clients.js

--- shubhchintak-india/src/pages/clients.js
+++ shubhchintak-india/src/pages/clients.js
@@ -3,45 +3,45 @@ import bg from "../../public/images/bg.png";
 import Marquee from "react-fast-marquee";
 function clients(){
     return(
-        <div className="bg-[#FFFFFF1A] max-[768px]:mt-[50%] mt-[2%] pb-[3%] bg-black-rgba pt-[.5%]">
+        <div className="bg-[#FFFFFF1A] max-[768px]:mt-[50%] mt-[2%]  bg-black-rgba pt-[.5%]">
             
-            <h2 className="font-semibold text-center text-[30px] mt-[5%] text-white font-abc">Our Esteemed Clients</h2>
-            <div className="flex flex-row justify-evenly mt-[5%]">
+            <h2 className="font-semibold text-center text-[30px] mt-[2%] text-white font-abc">Our Esteemed Clients</h2>
+            <div className="flex flex-row justify-evenly mt-[1%]">
                 <Marquee>
-            <div className="mr-[50%]">
+            <div className="w-[193] h-[69] mr-[250px] mt-[5%]">
                 <Image
                 alt=""
                 src="/images/logo1.png"
-                height={300}
-                width={300}/>
+                height={70}
+                width={195}/>
             </div>
-                <div className="mb-[3%] mr-[70%]">
+                <div className="w-[193] h-[69] mt-[-5%] mr-[250px]">
                 <Image
                 alt=""
                 src="/images/logo2.png"
-                height={400}
-                width={400}/>
+                height={94}
+                width={194}/>
             </div>
-            <div className="mt-[3%] mr-[70%]">
+            <div className="w-[280] h-[70] mr-[250px]">
                 <Image
                 alt=""
                 src="/images/logo3.png"
-                height={500}
-                width={500}/>
+                height={70}
+                width={280}/>
             </div>
-            <div className="mr-[70%]">
+            <div className="w-[186] h-[106] mr-[250px]">
                 <Image
                 alt=""
                 src="/images/logo4.png"
-                height={350}
-                width={350}/>
+                height={106}
+                width={186}/>
             </div>
-            <div className="mr-[70%]">
+            <div className="w-[177px] h-[125px] mr-[250px]">
                 <Image
                 alt=""
                 src="/images/logo5.png"
-                height={200}
-                width={200}/>
+                height={50}
+                width={80}/>
             </div>
             
             </Marquee>
