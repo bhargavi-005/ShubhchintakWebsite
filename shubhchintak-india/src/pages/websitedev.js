11c2d018a459dc38fd526641819faa20db9c704c
Chinmay Bhat <0310chinmay@gmail.com>
2023-05-06 16:39:09 +0530

    Update websitedev.js

--- shubhchintak-india/src/pages/websitedev.js
+++ shubhchintak-india/src/pages/websitedev.js
@@ -4,8 +4,8 @@ function web(){
     return(
         <div  className=" max-[768px]:flex max-[768px]:ml-[-2%]  order-last bg-gradient-to-r from-blue-400 to-purple-400 pt-[2%] mb-[-50%] mt-[5%] ">
             <div className=" max-[768px]:flex max-[768px]:flex-col flex flex-row ">
-                <div className="mt-[-3%] max-[768px]:order-last max-[768px]:mt-[2%] max-[768px]:ml-[-6%] mr-[8%]">
-                <h1 className="font-extrabold max-[768px]:text-center text-left ml-[18%] max-[768px]:text-[30px] text-white text-[50px] font-xyz">Website Development</h1>
+                <div className="mt-[-3%] max-[768px]:order-last max-[768px]:mt-[2%] max-[768px]:ml-[-8%] mr-[8%]">
+                <h1 className="font-extrabold max-[768px]:text-center text-left ml-[18%] max-[768px]:text-[30px] max-[768px]:ml-[15%] text-white text-[50px] font-xyz">Website Development</h1>
                 <p className="max-[768px]:text-center max-[768px]:ml-[15%] ml-[18%] mr-[%] text-white text-left max-[768px]:text-[14px] text-[20px]">With acceptance of  advance technology and prominent experience in the industry in providing successful digital solutions, we perform both front end and back end built for web development. We discuss, design, develop and deploy client based, goal and growth oriented web solutions. We have created websites for most challenging business ideas, this makes us different.</p>
                 <a href="">
                 <button class=" max-[768px]:ml-[70%] right-[26%] mt-[2%] relative bg-white hover:bg-blue-700 text-black font-bold py-2 px-4 rounded-full">
