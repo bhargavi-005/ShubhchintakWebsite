9aba5ce32e2bab24f7e891179c7ba0c45c26c8d4
sanjivanibhongade2001 <co.2019.sabhongade@bitwardha.ac.in>
2023-05-06 18:47:18 +0530

    Response

--- shubhchintak-india/src/pages/carouselUI.js
+++ shubhchintak-india/src/pages/carouselUI.js
@@ -1,110 +1,73 @@
-import React from "react";
-import Image from "next/image";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+import { Fragment, useState } from "react";
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
+import bg from "../../public/images/bg.png";
 
-export default function carouselUI() {
-
-  const CustomDot = ({ onClick, ...rest }) => {
-    const {
-      active,
-    } = rest;
-    return (
-      <button
-        className={active ? "w-[14px] h-[14px] rounded-[7px] bg-gray-600 mx-1 mt-10" : "w-[14px] h-[14px] rounded-[7px] bg-white mx-1 mt-10" }
-        onClick={() => onClick()}
-      />
-    );
-  };
-  
-   const responsive = {
-     superLargeDesktop: {
-       breakpoint: { max: 4000, min: 3000 },
-       items: 5,
-     },
-     desktop: {
-       breakpoint: { max: 3000, min: 1024 },
-       items: 3,
-     },
-     tablet: {
-       breakpoint: { max: 1024, min: 464 },
-       items: 2,
-     },
-     mobile: {
-       breakpoint: { max: 464, min: 0 },
-       items: 1,
-     },
-   };
-
-
-   
-
+function Icon({ id, open }) {
   return (
-    <div className="m-0">
-      <Carousel showDots={true} customDot={<CustomDot/>}  responsive={responsive} 
-      // customDot={<CustomDot/>}
-      >
-        <div className="p-2">
-        <div className="lg:max-w-lg lg:w-full md:w-1/3 w-5/6 pb-10 -mr-2 md:-mr-16 border-2">
-              <Image
-                className="w-full object-cover object-center rounded z-10"
-                alt="earth"
-                src="images/ECom.svg"
-                width={200}
-                height={200} 
-              />
-        </div>
-        <h1 className="text-center text-3xl font-bold mt-6">E-Commerce website</h1>
-        </div>
-        <div className="m-2">
-        <div className="lg:max-w-lg lg:w-full md:w-1/3 w-5/6 pb-10 -mr-2 md:-mr-16 border-2">
-              <Image
-                className="w-full object-cover object-center rounded z-10"
-                alt="earth"
-                src="images/game.svg"
-                width={200}
-                height={200} 
-              />
-        </div>
-        <h1 className="text-center text-3xl font-bold mt-6">Gaming Website</h1>
-        </div>
-        <div className="m-2">
-        <div className="lg:max-w-lg lg:w-full md:w-1/3 w-5/6 pb-10 -mr-2 md:-mr-16 border-2">
-              <Image
-                className="w-full object-cover object-center rounded z-10"
-                alt="earth"
-                src="images/Education.svg"
-                width={200}
-                height={200} 
-              />
-        </div>
-        <h1 className="text-center text-3xl font-bold mt-6">Education Website</h1>
-        </div>
-        <div className="m-2">
-        <div className="lg:max-w-lg lg:w-full md:w-1/3 w-5/6 pb-10 -mr-2 md:-mr-16 border-2">
-              <Image
-                className="w-full object-cover object-center rounded z-10"
-                alt="earth"
-                src="images/ECom.svg"
-                width={200}
-                height={200} 
-              />
-        </div>
-        <h1 className="text-3xl text-center font-bold mt-6">Entertainment Website</h1>
-        </div> 
-        <div className="m-2">
-        <div className="lg:max-w-lg lg:w-full md:w-1/3 w-5/6 pb-10 -mr-2 md:-mr-16 border-2">
-              <Image
-                className="w-full object-cover object-center rounded z-10"
-                alt="earth"
-                src="/images/game.svg"
-                width={200}
-                height={200} 
-              />
-        </div>
-        <h1 className="text-3xl text-center font-bold mt-6">Social Media Website</h1>
-        </div>
-      </Carousel>
-    </div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={`h-5 w-5 transition-transform ${
+        open === id ? "transform rotate-0" : ""
+      }`}
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      {open === id ? (
+        <>
+          <rect x="3" y="9" width="14" height="2" className="fill-current" />
+        </>
+      ) : (
+        <>
+          <rect x="9" y="3" width="2" height="14" className="fill-current" />
+          <rect x="3" y="9" width="14" height="2" className="fill-current" />
+        </>
+      )}
+    </svg>
   );
 }
+export default function Example() {
+  const [open, setOpen] = useState(0);
+
+  const handleOpen = (value) => {
+    setOpen(open === value ? 0 : value);
+  };
+
+  return (
+    <Fragment>
+      <div
+        className="text-white md:p-28 p-10 text-xl font-medium"
+        style={{ backgroundImage: `url(${bg.src})` }}
+      >
+      <div className="text-5xl font-bold text-center mt-16 mb-16">Frequently Asked Questions</div>
+      <div className="mt-[5%]" >
+        <Accordion open={open === 1} icon={<Icon id={1} open={open} />}>
+          <AccordionHeader onClick={() => handleOpen(1)}>
+            How can I do online business?
+          </AccordionHeader>
+          <AccordionBody>
+            <div className="text-[16px] text-white font-xyz mt-[2%] ">
+            1)Check visibility of our business idea by doing market research<br/>
+            2)Develop a business plan <br/>
+            3)Select attractive name of for business<br/>
+            4)Plan structure of your business<br/>
+            5)Build your website <br/>
+            6)Plan marketing strategy for growing your business.<br/>
+            Want to make it easier? Contact us, we will do all these steps for you.
+            </div>
+          </AccordionBody>
+        </Accordion>
+        </div>
+     
+
+        
+       
+      </div>
+    </Fragment>
+  );
+}
+
+
